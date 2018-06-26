7bac28579cfe7819bdac2947e8c61b81be48f63e
mabui <mabuix@gmail.com>
2018-06-26 21:21:04 +0900

    coinmarketcap api v2に対応

--- react_demo_app/src/index.js
+++ react_demo_app/src/index.js
@@ -9,29 +9,30 @@ class App extends React.Component {
 
     renderCoins() {
         // coinmarketcapから上位10位の銘柄取得api
-        let endpoint = "https://api.coinmarketcap.com/v1/ticker/?limit=10"
-        var list = [];
+        let endpoint = "https://api.coinmarketcap.com/v2/ticker/?limit=10&sort=rank"
         // fetchでリクエストを投げる。非同期処理でPromiseのオブジェクトが返却される。
+        let dict = {};
         fetch(endpoint).then((response) => {
             let json = response.json();
             json.then((value) => {
-                value.map((res) => {
-                    // レスポンスを配列に詰める。
-                    list.push(
-                        <li>
-                            {res['rank']} : {res['name']}, [price_usd: {res['price_usd']}], [market_cap_usd: {res['market_cap_usd']}], [percent_change_24h: {res['percent_change_24h']}]
-                        </li>
-
-                    );
-                }, (error) => {
-                    console.error(error);
-                })
+                let data = value.data;
+                for (let key in data) {
+                    let coin = data[key];
+                    let quotes_usd = coin['quotes']['USD'];
+                    // レスポンスを連想配列に詰める。
+                    dict[coin.rank] = <li>{coin['rank']} : {coin['name']}, [price_usd: {quotes_usd['price']}], [market_cap_usd: {quotes_usd['market_cap']}], [percent_change_24h: {quotes_usd['percent_change_24h']}]</li>
+                }
             })
         }, (error) => {
             console.error(error);
         }).then(() => {
-            // setTimeoutで非同期処理の結果(list)が帰ってくるのを待つ。
+            // setTimeoutで非同期処理の結果(dict)が帰ってくるのを待つ。
             setTimeout(() => {
+                let list = [];
+                // rank順に並び替え
+                for(let rank in dict) {
+                    list.push(dict[rank]);
+                }
                 // stateが更新されたら、render()が自動的に実行される。
                 this.setState({ list: list });
             }, 100)
